10668f8a666f46966c6eab6227ab9a00b63a0302
lenala <lena.lakhno@gmail.com>
2018-02-16 17:37:41 -0800

    Refactoring: common task

---
+++ azure-functions-gradle-plugin/src/main/java/com/microsoft/azure/gradle/functions/FunctionsTask.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+package com.microsoft.azure.gradle.functions;
+
+import com.microsoft.azure.gradle.functions.auth.AuthConfiguration;
+import com.microsoft.azure.gradle.functions.auth.AzureAuthFailureException;
+import com.microsoft.azure.gradle.functions.auth.AzureAuthHelper;
+import com.microsoft.azure.management.Azure;
+import org.gradle.api.DefaultTask;
+
+abstract class FunctionsTask extends DefaultTask implements AuthConfiguration {
+    public static final String AZURE_INIT_FAIL = "Failed to authenticate with Azure. Please check your configuration.";
+
+    protected AzureFunctionsExtension azureFunctionsExtension;
+    protected AzureAuthHelper azureAuthHelper;
+    private Azure azure;
+
+    public String getFinalName() {
+        return "finalName";
+//        return finalName;
+    }
+
+    @Override
+    public String getUserAgent() {
+        return getName() + " " + getGroup();
+//        return String.format("%s/%s %s:%s %s:%s", this.getName(), this.getGroup()
+//                getPluginName(), getPluginVersion(),
+//                INSTALLATION_ID_KEY, getInstallationId(),
+//                SESSION_ID_KEY, getSessionId());
+    }
+
+    @Override
+    public String getSubscriptionId() {
+        return (String) getProject().getProperties().get("subscriptionId");
+    }
+
+    @Override
+    public boolean hasAuthenticationSettings() {
+        return getProject().getProperties().containsKey(AzureAuthHelper.CLIENT_ID) || azureFunctionsExtension.getAuthFile() != null;
+    }
+
+    @Override
+    public String getAuthenticationSetting(String key) {
+        return (String) getProject().getProperties().get(key);
+    }
+
+    @Override
+    public String getAuthFile() {
+        return azureFunctionsExtension.getAuthFile();
+    }
+
+    public Azure getAzureClient() throws AzureAuthFailureException {
+        if (azure == null) {
+            azure = azureAuthHelper.getAzureClient();
+            if (azure == null) {
+//                getTelemetryProxy().trackEvent(INIT_FAILURE);
+                throw new AzureAuthFailureException(AZURE_INIT_FAIL);
+            } else {
+                // Repopulate subscriptionId in case it is not configured.
+//                getTelemetryProxy().addDefaultProperty(SUBSCRIPTION_ID_KEY, azure.subscriptionId());
+            }
+        }
+        return azure;
+    }
+
+    public String getBuildDirectoryAbsolutePath() {
+        return getProject().getBuildDir().getAbsolutePath();
+    }
+}
